8814b8df9db46a4eef816590c032076b7e14d174
murashkina2205 <150125681+murashkina2205@users.noreply.github.com>
2024-06-06 20:12:21 +0500

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById("navbar-toggler");
+        const menus = document.querySelectorAll(".menu");
+        btn.addEventListener('click', () => {
+            for(let el of menus) { 
+                el.classList.toggle("show");
+            }
+        })
